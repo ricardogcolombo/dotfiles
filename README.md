0882abd8f95cfbf471e586eca730ab64d4114ba1
Ricardo <R@Ricardo-3.local>
2018-01-08 19:49:59 -0300

    [ADD] Starting install script and readme

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # DOT FILES
+
+
+
+#FAQs and Errors
+
+##No Icons?
+- clone nerdfonts https://github.com/ryanoasis/nerd-fonts
+- instal nerdfonts with script 
+- select on Iterm> text > source code pro nerdfonts font(if it's not there check the fonts installation again')
+
+
+##load config
+Add to init.vim source file ~/dotfiles/init.vim
+
+##install plugin manager
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
--- install.sh
+++ install.sh
@@ -1,19 +1,38 @@
-update osx
-install xcode
-install git
+# update osx
+# install xcode
+xcode-select --install
 
-brew
+# brew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew doctor
+brew update
+brew install git
+# install python
+brew install python
+# pip
+curl -O http://python-distribute.org/distribute_setup.py
+python distribute_setup.py
+curl -O https://raw.github.com/pypa/pip/master/contrib/get-pip.py
+python get-pip.py
 
-pip
-pip2
-pip3
+# virtualenv
+pip install virtualenv
 
-neovim
-pluginstall
+# tmux
+brew install tmux
 
-install nvm
+# zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+#fonts
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+brew tap caskroom/fonts && brew cask install font-source-code-pro
 
-tmux
-zsh
+# neovim
+#install plugin manager
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# install nvm
+
 
 
