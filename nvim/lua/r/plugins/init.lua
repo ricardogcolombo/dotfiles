6ab37b3d8e878bcebbac7bacb6e45148c0d06cf4
ricardogcolombo <ricardogcolombo@gmail.com>
2023-12-19 17:05:55 -0600

    remove extras

--- nvim/lua/r/plugins/init.lua
+++ nvim/lua/r/plugins/init.lua
@@ -1,5 +1,5 @@
 return {
-  "nvim-lua/plenary.nvim", -- lua functions that many plugins use
-  "edeneast/nightfox.nvim",
-  "christoomey/vim-tmux-navigator", -- tmux & split window navigation
+	"nvim-lua/plenary.nvim", -- lua functions that many plugins use
+	"christoomey/vim-tmux-navigator", -- tmux & split window navigation
+	"edeneast/nightfox.nvim",
 }
