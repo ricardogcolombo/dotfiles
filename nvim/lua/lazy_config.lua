fedc57c1fcbc1522e44b1639752bb1008fc6342b
ricardogcolombo <ricardogcolombo@gmail.com>
2024-07-08 10:55:00 -0500

    adding updated config

--- nvim/lua/lazy_config.lua
+++ nvim/lua/lazy_config.lua
@@ -14,7 +14,7 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({ { import = "r.plugins" }, { import = "r.plugins.lsp" } }, {
 	install = {
-		colorscheme = { "nightfox" },
+		colorscheme = { "oxocarbon" },
 	},
 	checker = {
 		enabled = true,
--- nvim/lua/r/plugins/init.lua
+++ nvim/lua/r/plugins/init.lua
@@ -1,5 +1,6 @@
 return {
 	"nvim-lua/plenary.nvim", -- lua functions that many plugins use
 	"edeneast/nightfox.nvim",
+	"nyoom-engineering/oxocarbon.nvim",
 	"junegunn/fzf.vim",
 }
