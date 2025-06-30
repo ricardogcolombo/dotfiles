fd3eb547b1e769f642bf20f26cfc02ddb3aa89d9
ricardogcolombo <ricardogcolombo@gmail.com>
2025-06-30 14:41:10 -0500

    fixes to git

--- nvim/lua/r/plugins/fugitive.lua
+++ nvim/lua/r/plugins/fugitive.lua
@@ -3,5 +3,7 @@ return {
 	config = function()
 		vim.keymap.set("n", "<Leader>gc", ':Git commit -m ""<LEFT>')
 		vim.keymap.set("n", "<Leader>gs", ":Git<CR>")
+		vim.keymap.set("n", "<Leader>gp", ":Git push<CR>")
+		vim.keymap.set("n", "<Leader>gP", ":Git push -u origin HEAD<CR>")
 	end,
 }
