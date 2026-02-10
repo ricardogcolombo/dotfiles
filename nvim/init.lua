8baa7f2580cba7d4156afad3f19c6a9cb4dfceb4
ricardogcolombo <ricardogcolombo@gmail.com>
2026-02-10 17:48:52 -0600

    add changes in dotfiles

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,6 +3,7 @@ require("r")
 
 -- Load lazy.nvim plugin manager
 require("lazy_config")
+vim.opt.guifont = "OpenCode Nerd Font:h12" -- Use OpenCode GUI font 
 
 -- Set colorscheme
 vim.cmd("set background=dark")
